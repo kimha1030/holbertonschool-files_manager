df44ea40eed397893b07d400ccd1da8a0fc30bfc
kimha1030 <1478@holbertonschool.com>
2021-10-12 19:31:37 -0500

    Add directory utils that contains the redis.js, in this file we create the RedisClient class and the functions: isAlive, get, set and del

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.error(err);
+    });
+  }
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    const asynGet = promisify(this.client.get).bind(this.client);
+    const redisVal = await asynGet(key);
+    return redisVal;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+const redisClient = new RedisClient();
+export default redisClient;
